df306aa715596689974810ce9198aa446dc6a597
Vanja-Mijatov <vanja.mijatov@uns.ac.rs>
2017-06-18 19:09:32 +0200

    Tourist empty

---
+++ app/src/main/java/com/topguide/topguide/activity/TouristScreen.java
@@ -0,0 +1,15 @@
+package com.topguide.topguide.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.topguide.topguide.R;
+
+public class TouristScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tourist_screen);
+    }
+}
