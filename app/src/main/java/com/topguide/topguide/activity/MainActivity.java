6bd0dcc942eff00e40a68f93e4486536e477b1d8
itacca <igor.tica007@gmail.com>
2017-06-18 16:29:52 +0200

    "new package"

--- app/src/main/java/com/topguide/topguide/activity/MainActivity.java
+++ app/src/main/java/com/topguide/topguide/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.topguide.topguide;
+package com.topguide.topguide.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.topguide.topguide.R;
 
 public class MainActivity extends AppCompatActivity {
 
