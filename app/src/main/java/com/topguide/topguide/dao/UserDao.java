968d5a7ea60e956adfd12b9664223b588020ed0a
itacca <igor.tica007@gmail.com>
2017-06-25 12:59:21 +0200

    "test"

---
+++ app/src/main/java/com/topguide/topguide/dao/UserDao.java
@@ -0,0 +1,16 @@
+package com.topguide.topguide.dao;
+
+import com.topguide.topguide.model.User;
+
+/**
+ * Created by Igor on 6/20/2017.
+ */
+
+public class UserDao {
+
+    User currentUser;
+
+    public User getCurrentUser() {
+        return currentUser;
+    }
+}
---
+++ app/src/main/java/com/topguide/topguide/model/User.java
@@ -0,0 +1,9 @@
+package com.topguide.topguide.model;
+
+/**
+ * Created by Igor on 6/20/2017.
+ */
+
+public class User {
+
+}
