45ef9c3ab4f6c8bc291e010fca3803d8524e07e1
itacca <igor.tica007@gmail.com>
2017-06-30 00:34:52 +0200

    dopuna LoginActivity, RegisterActivity, WelcomeActivity i MainActivity - prijava na app

--- app/src/main/java/com/topguide/topguide/activity/RegisterActivity.java
+++ app/src/main/java/com/topguide/topguide/activity/RegisterActivity.java
@@ -1,15 +1,105 @@
 package com.topguide.topguide.activity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.topguide.topguide.R;
+import com.topguide.topguide.TopGuideApp;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText username;
+
+    private EditText password;
+
+    private EditText name;
+
+    private EditText lastname;
+
+    private EditText email;
+
+    private Button register;
+
+    private TopGuideApp app;
+
+    private boolean isSuccessful = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        app = (TopGuideApp) getApplication();
+
+        init();
+    }
+
+    private void init() {
+
+        username = (EditText) findViewById(R.id.username_register);
+        password = (EditText) findViewById(R.id.password_register);
+        name = (EditText) findViewById(R.id.name_register);
+        lastname = (EditText) findViewById(R.id.lastname_register);
+        email = (EditText) findViewById(R.id.email_register);
+
+        register = (Button) findViewById(R.id.register_button);
+
+        register.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        String textUsername = username.getText().toString();
+        String textPassword = password.getText().toString();
+        String textName = name.getText().toString();
+        String textLastName = lastname.getText().toString();
+        String textEmail = email.getText().toString();
+
+        if(textUsername == "" || textPassword == "" || textName == "" || textLastName == "" || textEmail == "" ) {
+            Context context = getApplicationContext();
+            CharSequence text = "Fields should not be empty!";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+            return;
+        }
+
+        if (app.getUserDao().userExists(textUsername)) {
+            Context con = getApplicationContext();
+            CharSequence text = "The user with the same username already exists!";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(con, text, duration);
+            toast.show();
+            return;
+        }
+
+        app.getUserDao().writeUser(textUsername, textPassword);
+
+        app.getPersonDao().writePerson(textUsername, textName, textLastName, textEmail);
+
+        isSuccessful = true;
+
+        setUpResult();
+    }
+
+    @Override
+    public void onBackPressed() {
+        setUpResult();
+        super.onBackPressed();
+    }
+
+    private void setUpResult() {
+        Intent intent = new Intent();
+        intent.putExtra("successful", isSuccessful);
+        setResult(RESULT_OK, intent);
     }
 }
