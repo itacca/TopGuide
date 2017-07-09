4d726e77648fa65bcb89573b182b1b8278964c94
filipbasara0 <basarafilip@gmail.com>
2017-07-09 15:34:56 +0200

    manje izmene

--- app/src/main/java/com/topguide/topguide/activity/TouristToursActivity.java
+++ app/src/main/java/com/topguide/topguide/activity/TouristToursActivity.java
@@ -2,6 +2,8 @@ package com.topguide.topguide.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ListView;
 
@@ -14,7 +16,6 @@ import com.topguide.topguide.model.Tour;
 import java.util.ArrayList;
 
 public class TouristToursActivity extends AppCompatActivity {
-    private ListView listView;
 
     private TopGuideApp app;
 
@@ -23,14 +24,23 @@ public class TouristToursActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tourist_tours);
 
+        app = (TopGuideApp) getApplication();
+
         init();
     }
 
     public void init() {
         ArrayList<Tour> tours = app.getPersonDao().getCurrentTourist().getTours();
 
-        listView = (ListView) findViewById(R.id.touristtourslist);
+        ListView listView = (ListView) findViewById(R.id.touristtourslist);
         TourAdapter adapter = new TourAdapter(this, tours);
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
     }
 }
